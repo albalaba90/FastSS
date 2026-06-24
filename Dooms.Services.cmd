bb54c58ede3812556bb620e6bbf81452266d32c3
albalaba90 <albalaba90@gmail.com>
2026-06-24 16:53:21 +0300

    Delete Dooms.Services.cmd

--- Dooms.Services.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit
-)
-powershell -Command "gps java,javaw -ea 0 | ft ProcessName,Id,@{N='Started';E={$_.StartTime.ToString('yyyy-MM-dd HH:mm:ss')}},@{N='Runtime';E={('{0:dd\.hh\:mm\:ss}' -f ((Get-Date)-$_.StartTime))}} -AutoSize"
-pause
